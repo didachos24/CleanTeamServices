7f5509b5b7d365643289f913ed55b78d4fbfb8b1
Diego F Montealegre Silva <didachos24@gmail.com>
2020-04-08 01:26:01 -0500

    Navbar style changed for responsive layout

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,8 +1,16 @@
-function navbarFunction () {
-    var x = document.getElementById("topnavbar");
-    if( x.className === "top-navbar") {
-        x.className += " responsive";
-    } else {
-        x.className = "top-navbar"
-    }
-}
+// function navbarFunction () {
+//     var x = document.getElementById("topnavbar");
+//     if( x.className === "top-navbar") {
+//         x.className += " responsive";
+//     } else {
+//         x.className = "top-navbar"
+//     }
+// }
+
+function openNav() {
+    document.getElementById("topnavbar").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("topnavbar").style.width = "0%";
+  }
