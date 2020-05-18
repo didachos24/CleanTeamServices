94998c62c9182b42f12fe45938be542db2194560
didachos24 <didachos24@gmail.com>
2020-05-18 01:12:04 -0500

    Slideshow created for landing page

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -6,3 +6,21 @@ function openNav() {
   function closeNav() {
     document.getElementById("topnavbar").style.width = "0%";
   }
+
+
+  // Slideshow 
+  var slideIndex = 0;
+  slideshow();
+
+  function slideshow() {
+    var i;
+    var slides = document.getElementsByClassName("slideshow");
+    console.log(slides.length);
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(slideshow, 4000);
+  }
