64d931f4944411d45eae58dc86366bc64f433673
didachos24 <didachos24@gmail.com>
2020-05-25 16:06:51 -0500

    Phone number added

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -13,13 +13,13 @@ function openNav() {
   slideshow();
 
   function slideshow() {
-    var i;
-    var slides = document.getElementsByClassName("slideshow");
+    let i;
+    let slides = document.getElementsByClassName("slideshow");
     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
     }
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
-    setTimeout(slideshow, 5000);
+    setTimeout(slideshow, 4000);
   }
