aeb1d0841b536e69d39fa185eb56abd4f9174e91
didachos24 <didachos24@gmail.com>
2020-05-26 01:27:38 -0500

    Content added

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,25 +1,35 @@
 // Responsive Navbar
 function openNav() {
+    // Show menu changing style
     document.getElementById("topnavbar").style.width = "100%";
   }
   
   function closeNav() {
+    // Hide menu
     document.getElementById("topnavbar").style.width = "0%";
   }
 
 
   // Slideshow 
+
+  // Slides counter
   var slideIndex = 0;
   slideshow();
 
   function slideshow() {
     let i;
+    // Catch pictures for slideshow
     let slides = document.getElementsByClassName("slideshow");
+    // Hide each picture in slideshow
     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
     }
+    // Start slideshow with slide counter, change picture
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
+    // Restart counter
+    if (slideIndex > slides.length) {slideIndex = 1};
+    // Change display for picture in turn
     slides[slideIndex-1].style.display = "block";
+    // Restart function every 4 seconds
     setTimeout(slideshow, 4000);
   }
